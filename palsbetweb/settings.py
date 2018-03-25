b146d1dcc46566cb727a26efb0a345679871c599
denis kiprono <deniskiprono4@gmail.com>
2018-03-25 18:24:32 +0300

    First version of application moved into github

--- palsbetweb/settings.py
+++ palsbetweb/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
-    os.path.join(palsbet, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 
