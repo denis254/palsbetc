f68bda5d46d0d8ce321053b03d75ed1747c89f06
denis kiprono <deniskiprono4@gmail.com>
2018-03-25 23:18:45 +0300

    First

--- palsbetweb/settings.py
+++ palsbetweb/settings.py
@@ -82,9 +82,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
