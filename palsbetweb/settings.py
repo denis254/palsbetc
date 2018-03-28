b880a0e7ac4a4161f599bd061af693e9b22175a5
denis kiprono <deniskiprono4@gmail.com>
2018-03-29 02:35:27 +0300

    F

--- palsbetweb/settings.py
+++ palsbetweb/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'palsbet',
 ]
 
@@ -48,7 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'palsbet.middleware.PreventConcurrentLoginsMiddleware'
+    'palsbet.middleware.PreventConcurrentLoginsMiddleware',
     
 
 
