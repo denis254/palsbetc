aa5f67d4ccb4420826c08b87fa2f031d6d9b9c89
denis kiprono <deniskiprono4@gmail.com>
2018-03-29 02:30:48 +0300

    F

--- palsbetweb/settings.py
+++ palsbetweb/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'palsbet.middleware.PreventConcurrentLoginsMiddleware'
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 
 
 ]
