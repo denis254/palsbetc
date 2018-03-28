2c69ae9b4e74b9c6c20eab0114d87e6a12be8553
denis kiprono <deniskiprono4@gmail.com>
2018-03-29 02:23:39 +0300

    First

--- palsbetweb/settings.py
+++ palsbetweb/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '(k#r1=fxip4p5+ku52s@oym%5yj%u=u-kz#uq#d056px*@8)('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['palsbet.herokuapp.com']
 
