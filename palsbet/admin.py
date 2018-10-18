d9778e30b04400cb3db15f5ad748736e129fd250
denis kiprono <deniskiprono4@gmail.com>
2018-10-18 10:41:11 +0300

    First

--- palsbet/admin.py
+++ palsbet/admin.py
@@ -1,19 +1,30 @@
 from django.contrib import admin
 
-from . models import FreeTipsGames,SingleBetGames,VipTips, PunterPick, RollOver, Notification
+from . models import FreeTipsGames,SingleBetGames,VipTips, PunterPick, RollOver
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(FreeTipsGames)
+@admin.register(SingleBetGames)
+class SingleBetGamesAdmin(ImportExportModelAdmin):
+    pass
 
-admin.site.register(SingleBetGames)
+@admin.register(FreeTipsGames)
+class FreeTipsGamesAdmin(ImportExportModelAdmin):
+    pass
 
-admin.site.register(VipTips)
+@admin.register(VipTips)
+class VipTipsAdmin(ImportExportModelAdmin):
+    pass
 
-admin.site.register(PunterPick)
+@admin.register(PunterPick)
+class PunterPickAdmin(ImportExportModelAdmin):
+    pass
 
-admin.site.register(RollOver)
+@admin.register(RollOver)
+class RollOverAdmin(ImportExportModelAdmin):
+    pass
 
-admin.site.register(Notification)
+
